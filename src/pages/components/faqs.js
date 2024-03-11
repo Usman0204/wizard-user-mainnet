46c8e46eca3c070383d4f471bb07c04de3f8cc73
hamzach625 <hamzach625@gmail.com>
2024-03-11 15:29:44 +0500

    changes

--- src/pages/components/faqs.js
+++ src/pages/components/faqs.js
@@ -41,8 +41,8 @@ const Faqs = () => {
                             <Accordion.Item eventKey="2">
                                 <Accordion.Header>3. What Fees does the platform charge?</Accordion.Header>
                                 <Accordion.Body>
-                                    At Wizard Gallery, we've kept our fee structure simple and transparent to ensure a seamless
-                                    experience for both creators and collectors. Here's a quick overview: <br /> <br />
+                                    At Wizard Gallery, we&apos;ve kept our fee structure simple and transparent to ensure a seamless
+                                    experience for both creators and collectors. Here&apos;s a quick overview: <br /> <br />
                                     Transaction Fees: We charge a modest fee of 2% on all transactions conducted on our platform.
                                     This fee is applied to sales, purchases, and trades of NFTs, ensuring that we can continue to
                                     provide a secure and innovative marketplace for our users. <br /><br />
