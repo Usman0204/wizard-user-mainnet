53fde93b7f47074e8602c27e02d5dfc854ca9003
m-jawa-d <chudhryjawad@gmail.com>
2024-03-19 20:05:42 +0500

    fixes

--- src/hooks/userSign.js
+++ src/hooks/userSign.js
@@ -197,23 +197,7 @@ const useSignature = (wab) => {
         library = getLibraryForSign(connectorsByName.walletconnect.provider);
       }
     }
-  // }
 
-  // if (web3 && account) {
-  //   // console.log("web3========================", web3,account)s
-  //   library = getLibraryForSign(web3?.givenProvider? web3?.givenProvider :wab?.givenProvider);
-  // }
-  // if (web3 && account) {
-  //   const connectorId = window.localStorage.getItem("connectorId")
-  //   if (connectorId === 'injected' && account) {
-  //     library = getLibraryForSign(web3?.givenProvider);
-  //   } else {
-  //     // return console.log(“connectorsByName”, connectorsByName[‘walletconnect’])
-  //     if (connectorsByName.walletconnect.provider) {
-  //       library = getLibraryForSign(connectorsByName.walletconnect.provider);
-  //     }
-  //   }
-  // }
 
 
   const sign = useCallback(
@@ -256,57 +240,3 @@ export default useSignature;
 
 
 
-
-// today
-// import { useWeb3React } from "@web3-react/core";
-// import { useCallback, useEffect } from "react";
-// // import Web3 from "web3";
-// import { getLibraryForSign } from "../utils/web3React";
-// import 'react-toastify/dist/ReactToastify.css';
-// import useWeb3 from "../hooks/useWeb3";
-// import { connectorsByName } from "../utils/web3React";
-
-// import { ethers } from "ethers";
-// export const Signature = (data) => {
-//   // let account = data;
-//   const { account } = useWeb3React()
-//   console.log(account);
-//   // const { account } = useWeb3React()
-//   const web3 = useWeb3()
-//   console.log(web3);
-//   var library = null;
-//   const connectorId = window.localStorage.getItem("connectorId")
-//   console.log(connectorId);
-//   if (connectorId === 'injected' && account) {
-//     console.log(web3?.givenProvider);
-//     library = getLibraryForSign(web3?.givenProvider);
-//   } else {
-//     if (connectorsByName.walletconnect.provider) {
-//       library = getLibraryForSign(connectorsByName.walletconnect.provider);
-//     }
-//   }
-//   const sign = useCallback(async (accountData) => {
-//     if ((library && account)) {
-//       try {
-//         if (account) {
-//           const connectorId = window.localStorage.getItem("connectorId")
-//           if (connectorId === 'injected') {
-//             library = getLibraryForSign(web3?.givenProvider);
-//           } else {
-//             library = getLibraryForSign(connectorsByName.walletconnect.provider);
-//           }
-//           let signature = await library.send(
-//             'personal_sign',
-//             [ethers.utils.hexlify(ethers.utils.toUtf8Bytes((account?.toLowerCase()))), account.toLowerCase()]
-//           );
-//           return signature
-//         }
-//       }
-//       catch (error) {
-//         throw error
-//       }
-//     }
-//   }, [account, library, data, web3])
-//   return { userSign: sign }
-// }
-// export default Signature
